7a0dbf8cec4ef5e343756a74b8e87818db3aa24b
Djoseffer <tom_djosefer@hotmail.com>
2023-10-01 12:46:10 -0300

    planet query tests

---
+++ sw-planet-api/src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
@@ -0,0 +1,58 @@
+package com.example.swplanetapi.domain;
+
+import static com.example.swplanetapi.common.PlanetConstants.INVALID_PLANET;
+import static com.example.swplanetapi.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.InstanceOfAssertFactories.OPTIONAL;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+public class PlanetServiceTest {
+  @InjectMocks
+  private PlanetService planetService;
+
+  @Mock
+  private PlanetRepository planetRepository;
+
+  @Test
+  public void createPlanet_WithValidData_ReturnsPlanet() {
+    when(planetRepository.save(PLANET)).thenReturn(PLANET);
+
+    Planet sut = planetService.create(PLANET);
+
+    assertThat(sut).isEqualTo(PLANET);
+  }
+
+  @Test
+  public void createPlanet_WithInvalidData_ThrowsException() {
+    when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+
+    assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+  }
+
+  @Test
+  public void getPlanet_ByExistingId_ReturnsPlanet() {
+    when(planetRepository.findById(1L)).thenReturn(Optional.of(PLANET));
+    Optional<Planet> sut = planetService.get(1L);
+    assertThat(sut).isNotEmpty();
+    assertThat(sut.get()).isEqualTo(PLANET);
+  }
+
+  @Test
+  public void getPlanet_ByUnexistingId_ReturnsEmpty() {
+    when(planetRepository.findById(1L)).thenReturn(Optional.empty());
+    Optional<Planet> sut = planetService.get(1L);
+
+    assertThat(sut).isEmpty();
+  }
+}
